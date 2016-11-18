8f09ed181430c3e42f9a95a20356f05cfc8b9717
Toki Sasaki From ACE10 <tochintosh56@gmail.com>
2016-11-18 13:38:57 +0900

    almost finish

---
+++ make-epub-src.bat
@@ -0,0 +1,4 @@
+cd ./src
+zip -0 -X ../epub-src.epub mimetype
+zip -r ../epub-src.epub * -x mimetype
+
--- make-epub.bat
+++ make-epub.bat
@@ -1,4 +1,4 @@
-cd .book-rev8-new
-zip-0 -X ../epub.epub mimetype
+cd ./dst
+zip -0 -X ../epub.epub mimetype
 zip -r ../epub.epub * -x mimetype
 
